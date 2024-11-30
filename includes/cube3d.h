d69c01368899864f8b41f9ca969313fe229467e1
hluiz-ma <hugollmarta@hotmail.com>
2024-11-30 18:49:50 +0000

    reorganize project

--- includes/cube3d.h
+++ includes/cube3d.h
@@ -1,21 +1,74 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cube.h                                             :+:      :+:    :+:   */
+/*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 16:39:31 by marsoare          #+#    #+#             */
-/*   Updated: 2024/11/29 16:40:40 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/11/30 18:48:41 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE_H
 # define CUBE_H
 
-#include <stdio.h>
-#include <math.h>
-#include <libft.h>
-#include <mlx.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include <string.h>
+# include <X11/keysym.h>
+# include <libft.h>
+# include <mlx.h>
+
+/* Window settings */
+# define WINDOW_WIDTH 1280
+# define WINDOW_HEIGHT 720
+# define FOV 60
+# define MOVE_SPEED 0.1
+# define ROTATION_SPEED 0.05
+
+/* Map characters */
+# define VALID_MAP_CHARS "01NSEW "
+# define PLAYER_CHARS "NSEW"
+
+/* Error messages */
+# define ERR_ARGS "Error\nInvalid number of arguments\n"
+# define ERR_FILE "Error\nInvalid file extension or unable to open file\n"
+# define ERR_MAP "Error\nInvalid map configuration\n"
+# define ERR_TEXTURE "Error\nInvalid texture configuration\n"
+# define ERR_COLOR "Error\nInvalid color configuration\n"
+# define ERR_MALLOC "Error\nMemory allocation failed\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
