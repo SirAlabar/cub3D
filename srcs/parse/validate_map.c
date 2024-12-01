93b8694be918cd94b8bf1a317b58f00460bf7bc4
MarkosComK <markoscomks@gmail.com>
2024-12-01 17:41:50 +0000

    Removed out of scope comment

--- srcs/parse/validate_map.c
+++ srcs/parse/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 17:15:56 by marsoare          #+#    #+#             */
-/*   Updated: 2024/12/01 17:37:23 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/12/01 17:41:38 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ bool	validate_map(t_game *game, int argc, char **argv)
 {
 	(void) game;
 	if (argc != 2)
-		return (printf(ERROR_USAGE), false);// <- add custom message later
+		return (printf(ERROR_USAGE), false);
 	if (!valid_extension(argv[1]))
 		return (printf(ERROR_USAGE ERROR_MAPTYPE), false);
 	return (true);
