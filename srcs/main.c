e29f812bfed8c1978e6057dabce3ed771f75f506
MarkosComK <markoscomks@gmail.com>
2024-12-01 16:50:30 +0000

    Started validations into main

--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,22 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 16:37:53 by marsoare          #+#    #+#             */
-/*   Updated: 2024/11/29 16:45:34 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/12/01 16:50:09 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cube3d.h>
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
 
-	(void)ac;
-	(void)av;
+	if (argc != 2)
+		printf("Error\n");
+	else
+		validate_map(argv);
 	printf("Setup\n");
-	ft_putendl_fd("setup?\n", 1);
 	mlx_ptr = mlx_init(); // Inicializa a conexão com a MLX
 	win_ptr = mlx_new_window(mlx_ptr, 300, 300, "Simple Screen");
 	mlx_loop(mlx_ptr);
