755939ce917169e078a39e53c1004bc5fd26cc83
hluiz-ma <hugollmarta@hotmail.com>
2025-01-01 16:57:16 +0000

    fixed leaks

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 16:37:53 by marsoare          #+#    #+#             */
-/*   Updated: 2025/01/01 16:36:27 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2025/01/01 16:55:52 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,32 @@ void cleanup_game(t_game *game)
 {
 	if (!game)
 		return;
-	if (game->win && game->mlx)
-		mlx_destroy_window(game->mlx, game->win);
+    if (game->win && game->mlx)
+    {
+        mlx_clear_window(game->mlx, game->win);
+        mlx_destroy_window(game->mlx, game->win);
+    }
+    if (game->img && game->mlx)
+        mlx_destroy_image(game->mlx, game->img);
+    #ifdef __linux__
+    if (game->mlx)
+    {
+        mlx_destroy_display(game->mlx);
+        free(game->mlx);
+    }
+    #endif    
 	free(game);
+}
+int close_window(t_game *game)
+{
+    mlx_loop_end(game->mlx);
+    return (0);
 }
 
 int key_handler(int keycode, t_game *game)
 {
-	(void) game;
 	if (keycode == KEY_ESC)
-	{
-		cleanup_game(game);
-		exit(0);
-	}
+        close_window(game);
 	return (0);
 }
 
@@ -41,13 +54,16 @@ int main(int argc, char **argv)
 		return (1);
 	if (!validate_map_extension(argc, argv))
 		return (free(game), 1);
+    game->img = NULL;    
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, 800, 600, "My new cube omg");
 
 	render_frame(game);
 	mlx_hook(game->win, 2, 1L<<0, key_handler, game);
+    mlx_hook(game->win, 17, 0, close_window, game); 
 	game_parse(game, argv[1]);
 	mlx_loop(game->mlx);
+    
 	cleanup_game(game);
 	return (0);
 }
--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 14:57:09 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2025/01/01 15:27:42 by hluiz-ma         ###   ########.fr       */
+/*   Updated: 2025/01/01 16:56:33 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,11 +49,16 @@ void draw_floor_ceiling(t_game *game)
 }
 void render_frame(t_game *game)
 {
+    if (game->img && game->mlx)
+    {
+        mlx_destroy_image(game->mlx, game->img);
+        game->img = NULL;
+    }
+    
     game->img = mlx_new_image(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
     game->addr = mlx_get_data_addr(game->img, &game->bits_per_pixel,&game->line_length, &game->endian);
     
     draw_floor_ceiling(game);
     
     mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
-    mlx_destroy_image(game->mlx, game->img);
 }
