bb8c2bd1749486acafd8c86cc4633e026577fde6
hluiz-ma <hugollmarta@hotmail.com>
2025-01-05 20:12:02 +0000

    rename folder

---
+++ srcs/initializer/init_game.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_game.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/01 18:03:35 by hluiz-ma          #+#    #+#             */
+/*   Updated: 2025/01/05 17:32:27 by hluiz-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cub3d.h>
+
+void	init_game(t_game *game)
+{
+	ft_memset(game, 0, sizeof(t_game));
+	game->map.floor_color = 0x808080;
+	game->map.ceiling_color = 0x87CEEB;
+	init_player(game);
+}
