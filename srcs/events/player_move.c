86690615ee9aa09c2fed51a0a67ea071b430343a
Alabar <hugollmarta@hotmail.com>
2025-01-10 21:01:41 +0000

    add normalize vector, improve the colide moviment with buffer and offset

--- srcs/events/player_move.c
+++ srcs/events/player_move.c
@@ -3,98 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   player_move.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 21:20:50 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2025/01/09 20:13:54 by marsoare         ###   ########.fr       */
+/*   Updated: 2025/01/10 20:43:43 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-void	move_player(t_game *game, double dir_x, double dir_y)
+bool	can_move_x(t_game *g, t_vector new, t_vector dir, double buff)
 {
-	double	new_x;
-	double	new_y;
+	int	new_grid_x_pos;
+	int	grid_y_pos;
+	int	offset;
 
-	new_x = game->p1.pos.x + dir_x * game->p1.move_speed;
-	new_y = game->p1.pos.y + dir_y * game->p1.move_speed;
-	if (game->map.grid[(int)new_x][(int)game->p1.pos.y] != '1')
-		game->p1.pos.x = new_x;
-	if (game->map.grid[(int)game->p1.pos.x][(int)new_y] != '1')
-		game->p1.pos.y = new_y;
+	new_grid_x_pos = (int)new.x;
+	grid_y_pos = (int)g->p1.pos.y;
+	if (dir.x > 0)
+		offset = 1;
+	else
+		offset = -1;
+	if (g->map.grid[new_grid_x_pos + offset * (int)buff][grid_y_pos] == '1')
+		return (false);
+	return (true);
 }
 
-void	rotate_player(t_game *g, double angle)
+bool	can_move_y(t_game *g, t_vector new, t_vector dir, double buff)
 {
-	double	old_dir_x;
-	double	old_plane_x;
+	int	new_grid_y_pos;
+	int	grid_x_pos;
+	int	offset;
 
-	old_dir_x = g->p1.dir.x;
-	old_plane_x = g->p1.plane.x;
-	g->p1.dir.x = g->p1.dir.x * cos(angle) - g->p1.dir.y * sin(angle);
-	g->p1.dir.y = old_dir_x * sin(angle) + g->p1.dir.y * cos(angle);
-	g->p1.plane.x = g->p1.plane.x * cos(angle) - g->p1.plane.y * sin(angle);
-	g->p1.plane.y = old_plane_x * sin(angle) + g->p1.plane.y * cos(angle);
+	new_grid_y_pos = (int)new.y;
+	grid_x_pos = (int)g->p1.pos.x;
+	if (dir.y > 0)
+		offset = 1;
+	else
+		offset = -1;
+	if (g->map.grid[grid_x_pos][new_grid_y_pos + offset * (int)buff] == '1')
+		return (false);
+	return (true);
 }
 
-void	handle_movement(t_game *game)
+void	move_player(t_game *g, double dir_x, double dir_y)
 {
-	double	dir_x;
-	double	dir_y;
+	t_vector	new_pos;
+	t_vector	dir;
+	double		buff;
 
-	dir_x = 0;
-	dir_y = 0;
-	if (game->p1.keys.w)
-	{
-		dir_x += game->p1.dir.x;
-		dir_y += game->p1.dir.y;
-	}
-	if (game->p1.keys.s)
-	{
-		dir_x -= game->p1.dir.x;
-		dir_y -= game->p1.dir.y;
-	}
-	keys_else(game, &dir_x, &dir_y);
-	move_player(game, dir_x, dir_y);
-	if (game->p1.keys.left)
-		rotate_player(game, game->p1.rot_speed);
-	if (game->p1.keys.right)
-		rotate_player(game, -game->p1.rot_speed);
+	buff = 0.1;
+	dir.x = dir_x;
+	dir.y = dir_y;
+	new_pos.x = g->p1.pos.x + dir.x * g->p1.move_speed;
+	new_pos.y = g->p1.pos.y + dir.y * g->p1.move_speed;
+	if (can_move_x(g, new_pos, dir, buff))
+		g->p1.pos.x = new_pos.x;
+	if (can_move_y(g, new_pos, dir, buff))
+		g->p1.pos.y = new_pos.y;
 }
 
-int	key_press(int keycode, t_game *game)
+void	rotate_player(t_game *game, double angle)
 {
-	if (keycode == KEY_ESC)
-		close_window(game);
-	else if (keycode == KEY_W)
-		game->p1.keys.w = 1;
-	else if (keycode == KEY_S)
-		game->p1.keys.s = 1;
-	else if (keycode == KEY_D)
-		game->p1.keys.d = 1;
-	else if (keycode == KEY_A)
-		game->p1.keys.a = 1;
-	else if (keycode == KEY_LEFT)
-		game->p1.keys.left = 1;
-	else if (keycode == KEY_RIGHT)
-		game->p1.keys.right = 1;
-	return (0);
-}
+	t_vector	old_dir;
+	t_vector	old_plane;
 
-int	key_release(int keycode, t_game *game)
-{
-	if (keycode == KEY_W)
-		game->p1.keys.w = 0;
-	else if (keycode == KEY_S)
-		game->p1.keys.s = 0;
-	else if (keycode == KEY_D)
-		game->p1.keys.d = 0;
-	else if (keycode == KEY_A)
-		game->p1.keys.a = 0;
-	else if (keycode == KEY_LEFT)
-		game->p1.keys.left = 0;
-	else if (keycode == KEY_RIGHT)
-		game->p1.keys.right = 0;
-	return (0);
+	old_dir = game->p1.dir;
+	old_plane = game->p1.plane;
+	game->p1.dir = vector_normalize(vector_rotate(old_dir, angle));
+	game->p1.plane = vector_rotate(old_plane, angle);
 }
