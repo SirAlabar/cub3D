c082222a0433233ba2249c7da8e1c05bc03879ee
Alabar <hugollmarta@hotmail.com>
2025-01-11 12:14:01 +0000

    improviment on visual buff to prevent zoom on wall moves

--- srcs/events/player_move.c
+++ srcs/events/player_move.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 21:20:50 by hluiz-ma          #+#    #+#             */
-/*   Updated: 2025/01/10 20:43:43 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/11 12:12:19 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 bool	can_move_x(t_game *g, t_vector new, t_vector dir, double buff)
 {
-	int	new_grid_x_pos;
-	int	grid_y_pos;
-	int	offset;
+	int		new_grid_x_pos;
+	int		grid_y_pos;
+	int		offset;
+	int		check_x;
+	double	dist_to_wall;
 
 	new_grid_x_pos = (int)new.x;
 	grid_y_pos = (int)g->p1.pos.y;
@@ -24,16 +26,28 @@ bool	can_move_x(t_game *g, t_vector new, t_vector dir, double buff)
 		offset = 1;
 	else
 		offset = -1;
-	if (g->map.grid[new_grid_x_pos + offset * (int)buff][grid_y_pos] == '1')
+	if (g->map.grid[new_grid_x_pos + offset * (int)0.2][grid_y_pos] == '1')
 		return (false);
+	check_x = new_grid_x_pos + offset;
+	if (g->map.grid[check_x][grid_y_pos] == '1')
+	{
+		if (dir.x > 0)
+			dist_to_wall = check_x - new.x;
+		else
+			dist_to_wall = new.x - new_grid_x_pos;
+		if (dist_to_wall < buff)
+			return (false);
+	}
 	return (true);
 }
 
 bool	can_move_y(t_game *g, t_vector new, t_vector dir, double buff)
 {
-	int	new_grid_y_pos;
-	int	grid_x_pos;
-	int	offset;
+	int		new_grid_y_pos;
+	int		grid_x_pos;
+	int		offset;
+	int		check_y;
+	double	dist_to_wall;
 
 	new_grid_y_pos = (int)new.y;
 	grid_x_pos = (int)g->p1.pos.x;
@@ -41,8 +55,18 @@ bool	can_move_y(t_game *g, t_vector new, t_vector dir, double buff)
 		offset = 1;
 	else
 		offset = -1;
-	if (g->map.grid[grid_x_pos][new_grid_y_pos + offset * (int)buff] == '1')
+	if (g->map.grid[grid_x_pos][new_grid_y_pos + offset * (int)0.2] == '1')
 		return (false);
+	check_y = new_grid_y_pos + offset;
+	if (g->map.grid[grid_x_pos][check_y] == '1')
+	{
+		if (dir.y > 0)
+			dist_to_wall = check_y - new.y;
+		else
+			dist_to_wall = new.y - new_grid_y_pos;
+		if (dist_to_wall < buff)
+			return (false);
+	}
 	return (true);
 }
 
@@ -50,16 +74,16 @@ void	move_player(t_game *g, double dir_x, double dir_y)
 {
 	t_vector	new_pos;
 	t_vector	dir;
-	double		buff;
+	double		visual_buff;
 
-	buff = 0.1;
+	visual_buff = 0.5;
 	dir.x = dir_x;
 	dir.y = dir_y;
 	new_pos.x = g->p1.pos.x + dir.x * g->p1.move_speed;
 	new_pos.y = g->p1.pos.y + dir.y * g->p1.move_speed;
-	if (can_move_x(g, new_pos, dir, buff))
+	if (can_move_x(g, new_pos, dir, visual_buff))
 		g->p1.pos.x = new_pos.x;
-	if (can_move_y(g, new_pos, dir, buff))
+	if (can_move_y(g, new_pos, dir, visual_buff))
 		g->p1.pos.y = new_pos.y;
 }
 
