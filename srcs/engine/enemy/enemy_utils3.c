fb6eded1c22af59e8b305606f8787452f469b822
MarkosComK <markoscomks@gmail.com>
2025-01-29 20:22:39 +0000

    Norm fix

--- srcs/engine/enemy/enemy_utils3.c
+++ srcs/engine/enemy/enemy_utils3.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 17:44:29 by marsoare          #+#    #+#             */
-/*   Updated: 2025/01/29 17:45:08 by marsoare         ###   ########.fr       */
+/*   Updated: 2025/01/29 20:22:31 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ bool	is_in_bounds(t_game *game, int x, int y)
 	return (x >= 0 && x < game->map.width && y >= 0 && y < game->map.height);
 }
 
-bool	has_hit_wall(t_game *game, t_ray_data *ray) {
-
+bool	has_hit_wall(t_game *game, t_ray_data *ray)
+{
 	return (is_in_bounds(game, ray->map_x, ray->map_y)
 		&& game->map.grid[ray->map_y][ray->map_x] == '1');
 }
