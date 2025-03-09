0463ab29b00339a9baf15190db101e9cc8047e4d
MarkosComK <markoscomks@gmail.com>
2025-03-09 11:48:34 +0000

    Norm fix

--- srcs/engine/enemy/enemy_utils2.c
+++ srcs/engine/enemy/enemy_utils2.c
@@ -6,48 +6,93 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 10:16:16 by marsoare          #+#    #+#             */
-/*   Updated: 2025/02/24 18:05:02 by marsoare         ###   ########.fr       */
+/*   Updated: 2025/03/09 11:47:16 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-bool	is_enemy_visible(t_game *game, t_vector enemy_pos)
+static bool	check_short_distance(t_game *game, t_vector enemy_pos)
 {
-	t_ray_data	ray;
-	int			max_steps;
-	int			step_count;
-	double		distance;
+	double	distance;
 
 	distance = vector_dist(game->p1.pos, enemy_pos);
 	if (distance < 1.5)
 		return (true);
 	if (!is_in_bounds(game, enemy_pos.x, enemy_pos.y))
 		return (false);
-	ray = init_ray_data(game, enemy_pos);
-	max_steps = (int)(distance * 2) + 10;
+	return (false);
+}
+
+static bool	ray_trace_to_enemy(t_game *game, t_ray_data *ray,
+	t_vector enemy_pos, int max_steps)
+{
+	int	step_count;
+
 	step_count = 0;
 	while (step_count < max_steps)
 	{
 		step_count++;
-		if ((int)ray.map_x == (int)enemy_pos.x && (int)ray.map_y == (int)enemy_pos.y)
+		if ((int)ray->map_x == (int)enemy_pos.x
+			&& (int)ray->map_y == (int)enemy_pos.y)
 			return (true);
-		if (has_hit_wall(game, &ray))
+		if (has_hit_wall(game, ray))
 			return (false);
-		if (!is_in_bounds(game, ray.map_x, ray.map_y))
+		if (!is_in_bounds(game, ray->map_x, ray->map_y))
 			return (false);
-		if (ray.side_dist.x < ray.side_dist.y)
+		if (ray->side_dist.x < ray->side_dist.y)
 		{
-			ray.side_dist.x += ray.delta_dist.x;
-			ray.map_x += ray.step.x;
+			ray->side_dist.x += ray->delta_dist.x;
+			ray->map_x += ray->step.x;
 		}
 		else
 		{
-			ray.side_dist.y += ray.delta_dist.y;
-			ray.map_y += ray.step.y;
+			ray->side_dist.y += ray->delta_dist.y;
+			ray->map_y += ray->step.y;
 		}
 	}
 	return (false);
+}
+
+bool	is_enemy_visible(t_game *game, t_vector enemy_pos)
+{
+	t_ray_data	ray;
+	int			max_steps;
+	double		distance;
+
+	if (check_short_distance(game, enemy_pos))
+		return (true);
+	distance = vector_dist(game->p1.pos, enemy_pos);
+	ray = init_ray_data(game, enemy_pos);
+	max_steps = (int)(distance * 2) + 10;
+	return (ray_trace_to_enemy(game, &ray, enemy_pos, max_steps));
+}
+
+static void	adjust_angle_and_fov(double *angle, double *effective_fov,
+	double fov, double dist)
+{
+	double	fov_scale;
+
+	while (*angle < -M_PI)
+		*angle += 2 * M_PI;
+	while (*angle > M_PI)
+		*angle -= 2 * M_PI;
+	*effective_fov = fov;
+	if (dist < 2.0)
+	{
+		fov_scale = 1.0 + ((2.0 - dist) / 4.0);
+		*effective_fov = fov * fov_scale;
+	}
+}
+
+static int	calculate_sprite_height(double dist)
+{
+	int	sprite_height;
+
+	sprite_height = (int)(WINDOW_HEIGHT / dist);
+	if (sprite_height > WINDOW_HEIGHT * 1.5)
+		sprite_height = WINDOW_HEIGHT * 1.5;
+	return (sprite_height);
 }
 
 void	draw_enemy(t_game *game, t_enemy_list *current, double fov)
@@ -56,26 +101,24 @@ void	draw_enemy(t_game *game, t_enemy_list *current, double fov)
 	double		angle;
 	double		dist;
 	double		screen_x;
-	int			sprite_height;
-	double		effective_fov = fov;
+	double		effective_fov;
 
 	rel_pos = vector_sub(current->enemy.pos, game->p1.pos);
 	dist = vector_length(rel_pos);
-	angle = atan2(rel_pos.y, rel_pos.x) - atan2(game->p1.dir.y, game->p1.dir.x);
-	while (angle < -M_PI)
-		angle += 2 * M_PI;
-	while (angle > M_PI)
-		angle -= 2 * M_PI;
-	if (dist < 2.0)
-	{
-		double fov_scale = 1.0 + ((2.0 - dist) / 4.0);
-		effective_fov = fov * fov_scale;
-	}
+	angle = atan2(rel_pos.y, rel_pos.x)
+		- atan2(game->p1.dir.y, game->p1.dir.x);
+	adjust_angle_and_fov(&angle, &effective_fov, fov, dist);
 	screen_x = (angle + effective_fov / 2) * (WINDOW_WIDTH / effective_fov);
-	sprite_height = (int)(WINDOW_HEIGHT / dist);
-	if (sprite_height > WINDOW_HEIGHT * 1.5)
-		sprite_height = WINDOW_HEIGHT * 1.5;
-	draw_enemy_sprite(game, &current->enemy, screen_x, sprite_height);
+	draw_enemy_sprite(game, &current->enemy, screen_x,
+		calculate_sprite_height(dist));
+}
+
+static void	check_and_draw_enemy(t_game *game, t_enemy_list *current, double fov)
+{
+	if (vector_dist(game->p1.pos, current->enemy.pos) < 2.0)
+		draw_enemy(game, current, fov);
+	else if (is_enemy_visible(game, current->enemy.pos))
+		draw_enemy(game, current, fov);
 }
 
 void	draw_enemies(t_game *game)
@@ -88,16 +131,7 @@ void	draw_enemies(t_game *game)
 	while (current != NULL)
 	{
 		if (current->enemy.alive)
-		{
-			if (vector_dist(game->p1.pos, current->enemy.pos) < 2.0)
-			{
-				draw_enemy(game, current, fov);
-			}
-			else if (is_enemy_visible(game, current->enemy.pos))
-			{
-				draw_enemy(game, current, fov);
-			}
-		}
+			check_and_draw_enemy(game, current, fov);
 		current = current->next;
 	}
 }
