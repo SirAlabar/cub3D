51765e5f2efc15a79d7b2758c7009594d6bf2fa2
hluiz-ma <hugollmarta@hotmail.com>
2025-03-22 20:11:46 +0000

    fix draw minimap

--- srcs/engine/minimap/minimap_draw.c
+++ srcs/engine/minimap/minimap_draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minimap_draw.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: hluiz-ma <hluiz-ma@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 14:20:40 by marsoare          #+#    #+#             */
-/*   Updated: 2025/01/16 20:27:54 by marsoare         ###   ########.fr       */
+/*   Updated: 2025/03/22 20:08:54 by hluiz-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	draw_minimap_cell(t_game *game, int screen_x, int screen_y, int color)
 	int	i;
 	int	j;
 
-	start_x = MINIMAP_PADDING + (screen_x * MINIMAP_CELL_SIZE);
-	start_y = WINDOW_HEIGHT - MINIMAP_PADDING - ((MINIMAP_VIEW_SIZE - screen_y)
-			* MINIMAP_CELL_SIZE);
+    start_x = MINIMAP_PADDING + (screen_x * MINIMAP_CELL_SIZE);
+    start_y = WINDOW_HEIGHT - MINIMAP_PADDING - ((MINIMAP_VIEW_SIZE - screen_y)
+            * MINIMAP_CELL_SIZE);
 	i = -1;
 	while (++i < MINIMAP_CELL_SIZE)
 	{
@@ -51,8 +51,8 @@ void	draw_direction_line(t_game *game, t_vector center, t_vector dir,
 	int			steps;
 	int			i;
 
-	end.x = center.x + dir.y * length;
-	end.y = center.y + dir.x * length;
+	end.x = center.x + dir.x * length;
+	end.y = center.y + dir.y * length;
 	steps = get_line_steps(center, end);
 	inc.x = (end.x - center.x) / (float)steps;
 	inc.y = (end.y - center.y) / (float)steps;
